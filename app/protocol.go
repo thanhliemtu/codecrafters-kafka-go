6b010f13c9cdc03a369f52629b789aff7215c709
Thanh Liem Tu <engt1@tme-inc.com>
2026-04-23 16:52:08 -0400

    codecrafters submit [skip ci]

--- app/protocol.go
+++ app/protocol.go
@@ -233,7 +233,7 @@ func handleDescribeTopicPartitions(frame Frame, header RequestHeaderV2) (respons
 	body = append(body, 2) // topics array length: 1 element (1 byte)
 
 	body = binary.BigEndian.AppendUint16(body, uint16(error_code))           // error_code: 3 (2 bytes)
-	body = append(body, 4)                                                   // name length: 3 (compact string) (1 byte)
+	body = binary.AppendUvarint(body, uint64(len(topic)+1))                  // name length: 3 (compact string) (1 byte)
 	body = append(body, topic...)                                            // topic_name: "foo" (3 bytes)
 	body = append(body, "0000000000000000"...)                               // topic_id: 0000000000000000 (16 bytes)
 	body = append(body, 0)                                                   // is_internal: false (1 byte)
